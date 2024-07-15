2a2c6f8bfabf958a5d2d2f50e25e56002a6fef28
hyomin1 <leehm123456@gmail.com>
2024-07-15 19:54:34 +0900

    사진업로드명 Date.now() -> uid 변경

--- backend/src/middleware/imageUploader.ts
+++ backend/src/middleware/imageUploader.ts
@@ -1,6 +1,7 @@
 import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
 
 const s3 = new S3Client({
   credentials: {
@@ -17,7 +18,7 @@ const imageUpload = multer({
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, callback) => {
       console.log("d", req.file);
-      callback(null, `${Date.now()}_${file.originalname}`);
+      callback(null, `${uuidv4()}_${file.originalname}`);
     },
     acl: "public-read",
   }),
